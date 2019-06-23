d60d41ff704d13daf9c867b044250cafec9c7719
mikeygbd <mikey.soares@gmail.com>
2019-06-22 22:55:50 -0400

    scaffolded users temporarily

---
+++ db/migrate/20190623021656_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.string :password_digest
+      t.string :firstname
+      t.string :lastname
+      t.string :location
+      t.timestamps
+    end
+  end
+end
