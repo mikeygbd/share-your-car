a6535349e268ac655ec7d1f18934664aa23018ef
mikeygbd <mikey.soares@gmail.com>
2019-06-23 14:37:10 -0400

    added fast_json syntax to users controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -5,12 +5,14 @@ class Api::UsersController < ApplicationController
   def index
     @users = User.all
 
-    render json: @users
+    user_json = UserSerializer.new(@users).serialized_json
+    render json: user_json
   end
 
   # GET /users/1
   def show
-    render json: @user
+    user_json = UserSerializer.new(@user).serialized_json
+    render json: user_json
   end
 
   # POST /users
