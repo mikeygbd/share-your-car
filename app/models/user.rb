87070e1371d68cb5fb34cc73e30d88950d160ae9
mikeygbd <mikey.soares@gmail.com>
2019-06-23 10:42:57 -0400

    changes has_many relationship to have the foreign key of customer-id with the classname of Reservations and added has many reviews

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   has_secure_password
   has_many :cars
   belongs_to :hometown, class_name: "Location"
+  has_many :reservations, foreign_key: :customer_id, class_name: "Reservation"
+  has_many :reviews
 end
