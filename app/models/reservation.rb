0fc450dfb31b58822d6d6433dcf23a6f3372fa8f
mikeygbd <mikey.soares@gmail.com>
2019-06-23 11:31:27 -0400

    added belongs_to owner with the class_name of User

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,5 +1,6 @@
 class Reservation < ApplicationRecord
   belongs_to :customer, class_name: "User"
+  belongs_to :owner, class_name: "User"
   belongs_to :car
   has_one :location, through: :car
 end
