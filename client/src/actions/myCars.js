1defbc3725b19f571b9c2b460a50674179153680
mikeygbd <mikey.soares@gmail.com>
2019-06-24 17:07:41 -0400

    created a getMyCars async action that fetches from the users cars url to the sessions method get_my_cars then dispatches setMyCars

--- client/src/actions/myCars.js
+++ client/src/actions/myCars.js
@@ -1,6 +1,31 @@
+//sync
 export const setMyCars = cars => {
   return {
     type: "SET_MY_CARS",
     cars
+  }
+}
+
+//async
+export const getMyCars = () => {
+  return dispatch => {
+    return fetch(`http://localhost:3001/api/users/:id/cars`, {
+      credentials: "include",
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      },
+    })
+    .then(r => r.json())
+    .then(response => {
+      if (response.error) {
+        alert(response.error)
+      } else {
+        console.log(response)
+        dispatch(setMyCars(response))
+      }
+    })
+    .catch(console.log)
+
   }
 }
