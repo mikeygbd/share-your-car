75b2bd1ff192df0d541b470e7219512f7abaf124
mikeygbd <mikey.soares@gmail.com>
2019-06-23 23:06:09 -0400

    added post /login and get /get_current_user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
+
   namespace :api do
+    post '/login', to: "sessions#create"
+    get '/get_current_user', to: "sessions#get_current_user"
+
   resources :users
   resources :cars
   resources :locations
