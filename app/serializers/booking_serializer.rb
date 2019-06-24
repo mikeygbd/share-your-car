83c0c31ad8a9417fdcb10299bfbe4827a982847f
mikeygbd <mikey.soares@gmail.com>
2019-06-24 15:10:59 -0400

    created car serializer that has many reservations and belongs to location and an owner

--- app/serializers/booking_serializer.rb
+++ app/serializers/booking_serializer.rb
@@ -1,3 +1,4 @@
 class BookingSerializer < ActiveModel::Serializer
   attributes :id, :start_date, :end_date, :created_at, :updated_at, :customer_id, :owner_id, :car_id
+  
 end
---
+++ app/serializers/car_serializer.rb
@@ -0,0 +1,6 @@
+class CarSerializer < ActiveModel::Serializer
+  attributes :id, :owner_id, :description, :make, :model, :year, :milage, :car_type, :total_passengers, :daily_rate, :weekly_rate, :monthly_rate, :location_id, :img
+  has_many :reservations, serializer: ReservationSerializer
+  belongs_to :location, serializer: LocationSerializer
+  belongs_to :owner, serializer: OwnerSerializer
+end
