10986293a2339689eab3421153141d7832bbb89a
mikeygbd <mikey.soares@gmail.com>
2019-06-24 17:11:08 -0400

    created a custom route to my_cars method in sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   post '/login', to: "sessions#create"
   get '/get_current_user', to: "sessions#get_current_user"
   delete '/logout', to: "sessions#destroy"
+  get '/users/:id/cars', to: "sessions#my_cars"
 
   resources :users
   resources :cars
