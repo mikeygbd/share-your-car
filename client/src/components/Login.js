75b324375953b91fa1aa1bee43368926d523d235
mikeygbd <mikey.soares@gmail.com>
2019-06-25 17:23:30 -0400

    added material ui to login.js

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -2,8 +2,31 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { updateLoginForm } from '../actions/loginForm'
 import { login } from '../actions/currentUser'
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: 130,
+  },
+  dense: {
+    marginTop: 19,
+  },
+  menu: {
+    width: 200,
+  },
+}));
 
 const Login = ({ loginForm, updateLoginForm, login }) => {
+  const classes = useStyles();
+
 
   const handleInputChange = event => {
     const { name, value } = event.target
@@ -22,9 +45,29 @@ const Login = ({ loginForm, updateLoginForm, login }) => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <input type="text" name="email" placeholder="Email" value={loginForm.email} onChange={handleInputChange} />
-      <input type="password" name="password" placeholder="Password" value={loginForm.password} onChange={handleInputChange} />
-      <input type="submit" value="Log In" />
+      <TextField
+        id="standard-with-placeholder"
+        type="text"
+        name="email"
+        placeholder="Email"
+        className={classes.textField}
+        value={loginForm.email}
+        onChange={handleInputChange}
+
+      />
+      <TextField
+         id="standard-password-input"
+         name="password"
+         className={classes.textField}
+         type="password"
+         placeholder="Password"
+         autoComplete="current-password"
+         value={loginForm.password}
+         onChange={handleInputChange}
+       />
+     <Button variant="contained" size="small" color="primary" type="submit" className={classes.margin}>
+            Login
+          </Button>
     </form>
   )
 }
