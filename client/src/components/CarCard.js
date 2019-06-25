78299b52d25df3be4d8ce1a4989c16b608db4658
mikeygbd <mikey.soares@gmail.com>
2019-06-25 17:22:23 -0400

    created a car card with material ui

--- client/src/components/CarCard.js
+++ client/src/components/CarCard.js
@@ -1,11 +1,130 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { grey } from '@material-ui/core/colors';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
+const useStyles = makeStyles(theme => ({
+  card: {
+    backgroundColor: grey[400],
+    color: 'white'[500],
+    width: 345,
+    height: 400,
+    margin: 20,
+
+  },
+  typo: {
+
+  },
+  cardHeader: {
+    textAlign: 'left',
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%',
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  avatar: {
+    backgroundColor: 'white'[500],
+  },
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}))
 
 
 const CarCard = ({ car }) => {
+  const classes = useStyles()
+
+  const [expanded, setExpanded] = React.useState(false)
+  const title = `${car.make} ${car.model}`
+  function handleExpandClick() {
+    setExpanded(!expanded)
+  }
+
   return (
-    
-    <p>{car.model}</p>
+
+    <Card id="car-card" className={classes.card} >
+      <CardHeader className={classes.cardHeader}
+        avatar={
+          <Avatar aria-label="Owner Img" className={classes.avatar} image={car.owner.img}>
+            <img className="owner-img" src={car.owner.img}/>
+          </Avatar>
+        }
+        action={
+         <IconButton aria-label="Settings">
+           <MoreVertIcon />
+         </IconButton>
+       }
+       title={title}
+       subheader={car.year}/>
+        <CardMedia
+          className={classes.media}
+          image={car.img}
+          title= {title}
+          />
+      <CardContent>
+        <Typography variant="body2" component="p">
+          {car.description}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="Add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="Share">
+          <ShareIcon />
+        </IconButton>
+        <IconButton
+       className={clsx(classes.expand, {
+         [classes.expandOpen]: expanded,
+       })}
+       onClick={handleExpandClick}
+       aria-expanded={expanded}
+       aria-label="Show more"
+     >
+       <ExpandMoreIcon />
+     </IconButton>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography>
+          Car Milage: {car.milage}<br />
+        Daily Rate: ${car.daily_rate}<br />
+      Weekly Rate: ${car.weekly_rate}<br />
+    Monthly Rate: ${car.monthly_rate}<br />
+          </Typography>
+        </CardContent>
+    </Collapse>
+    </Card>
+
+
   )
 }
 
