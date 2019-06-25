4684d011cb1c1fdef3eb6756167cee251c48b805
mikeygbd <mikey.soares@gmail.com>
2019-06-25 17:25:02 -0400

    imported both cars and carcss and deleted my cars to render them on a profile page

--- client/src/components/MainContainer.js
+++ client/src/components/MainContainer.js
@@ -1,12 +1,29 @@
 import React from 'react'
 import MyCars from './MyCars'
+import Cars from './Cars'
+import carCss from '../styles/car.css'
+import { connect } from 'react-redux'
 
-const MainContainer = () => {
+
+const MainContainer = ({ currentUser, cars}) => {
   return (
   <div className="MainContainer">
-    <MyCars/>
+  
+    <br />
+    <div className="Cars">
+    <Cars/>
+    </div>
+
+
   </div>
   )
 }
 
-export default MainContainer
+const mapStateToProps = ({currentUser, cars}) => {
+  return {
+    currentUser,
+    cars
+  }
+}
+
+export default connect(mapStateToProps)(MainContainer)
