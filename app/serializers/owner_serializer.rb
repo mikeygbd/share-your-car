f33030c5f6dd276664e4e5f27dd054efdd1032c7
mikeygbd <mikey.soares@gmail.com>
2019-06-25 17:19:13 -0400

    added img to owner serializer

--- app/serializers/owner_serializer.rb
+++ app/serializers/owner_serializer.rb
@@ -1,4 +1,4 @@
 class OwnerSerializer < ActiveModel::Serializer
-  attributes :id, :email, :firstname, :lastname, :hometown
+  attributes :id, :email, :firstname, :lastname, :hometown, :img
   has_many :cars, serializer: CarSerializer
 end
