e591a7a9f4fee8a78a15eaf0196d6277645acbba
mikeygbd <mikey.soares@gmail.com>
2019-06-25 17:19:28 -0400

    added img to user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :firstname, :lastname, :hometown
+  attributes :id, :email, :firstname, :lastname, :hometown, :img
   has_many :cars, serializer: CarSerializer
   has_many :reservations, serializer: ReservationSerializer
   has_many :bookings, serializer: BookingSerializer
