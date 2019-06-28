25ad4afed22dd0fde7cb2997dfb2558aa9b01595
mikeygbd <mikey.soares@gmail.com>
2019-06-28 16:28:41 -0400

    created MyBokings container

---
+++ client/src/components/MyBookings.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import BookingCard from './BookingCard'
+import { connect } from 'react-redux'
+
+
+const MyBookings = ({ myBookings, currentUser}) => {
+  const bookingCards = myBookings.length > 0 ? myBookings.map(b => <BookingCard booking={b} key={b.id}/>) : null
+
+  return (
+    <div className="MyBookings">
+
+     {bookingCards}
+    </div>
+  )
+}
+
+const mapStateToProps = state => {
+  return {
+    myBookings: state.myBookings,
+    currentUser: state.currentUser
+
+  }
+}
+
+export default connect(mapStateToProps)(MyBookings)
