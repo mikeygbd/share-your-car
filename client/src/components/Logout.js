7f97bef5308a32818abd541c780fa672929bcd00
mikeygbd <mikey.soares@gmail.com>
2019-06-28 09:11:03 -0400

    imported withRouter to grab history prop and push to homepage. created form and added div for background

--- client/src/components/Logout.js
+++ client/src/components/Logout.js
@@ -2,19 +2,35 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { logout } from '../actions/currentUser'
 import Button from '@material-ui/core/Button';
+import '../styles/registration.css'
+import Toolbar from '@material-ui/core/Toolbar';
+import { withRouter } from 'react-router-dom';
 
+const Logout = ({ history, logout }) => {
+    const handleSubmit = event => {
 
-const Logout = ({ logout }) => {
-
+    event.preventDefault()
+    logout()
+    history.push('/')
+  }
 
 
   return (
-    <form onSubmit={logout}>
-      <Button type="submit" variant="contained" size="small" color="primary" >
-             Logout
-           </Button>
+    <div className="Logout">
+    <div className="UserForm">
+    <div className="UserForm-title">
+      <Toolbar>Are You Sure You Want To Log Out?</Toolbar>
+    </div>
+    <form className="logout-form" onSubmit={logout}>
+      <div className="logout-btn">
+        <Button  variant="contained" size="small" color="primary" type="submit" onSubmit={handleSubmit} >
+           Logout
+        </Button>
+      </div>
     </form>
+  </div>
+  </div>
   )
 }
 
-export default connect(null, { logout })(Logout)
+export default withRouter(connect(null, { logout })(Logout))
