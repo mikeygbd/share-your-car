e04ef108947303ee6390c0afafe0560387571530
mikeygbd <mikey.soares@gmail.com>
2019-06-28 08:56:41 -0400

    created a find or createby with the users hometown info and set hometown to that location

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -17,11 +17,17 @@ class Api::UsersController < ApplicationController
   # POST /users
   def create
     @user = User.new(user_params)
+    @location = Location.find_or_create_by(city: params[:user][:hometown][:city], state: params[:user][:hometown][:state], country: params[:user][:hometown][:country])
+    @user.hometown = @location
 
     if @user.save
-      render json: @user, status: :created, location: @user
+      session[:user_id] = @user.id
+      render json: @user, status: :created
     else
-      render json: @user.errors, status: :unprocessable_entity
+      resp = {
+        error: @user.errors.full_messages.to_sentence
+      }
+      render json: resp, status: :unprocessable_entity
     end
   end
 
