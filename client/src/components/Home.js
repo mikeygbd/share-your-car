dd72dbd007ad12cd8dc0afa9abd6c6c011aa2c57
mikeygbd <mikey.soares@gmail.com>
2019-06-28 09:08:12 -0400

    imported logo and set up welcome screen for homepage

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import logo from '../images/white_transparent_1.png';
 
 const Home = () => {
   return (
 
     <div className="Home">
-    <h2>Home Page</h2>
-    </div>
+        <div className="welcome-card">
+          <div className='welcome-msg'>Welcome To</div>
+          <div><img src={logo} alt="Logo" width="300"/></div>
+          <div className='welcome-sub-msg'>Let Your Car Work For You!</div>
+        </div>
+      </div>
+
   )
 }
 
