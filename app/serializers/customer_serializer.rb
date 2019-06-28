33de796c4d85cc897d3668f28ac465240d1249f8
mikeygbd <mikey.soares@gmail.com>
2019-06-28 16:26:20 -0400

    added img attribute

--- app/serializers/customer_serializer.rb
+++ app/serializers/customer_serializer.rb
@@ -1,4 +1,4 @@
 class CustomerSerializer < ActiveModel::Serializer
-  attributes :id, :email, :firstname, :lastname, :hometown
+  attributes :id, :email, :firstname, :lastname, :hometown, :img
   has_many :reservations, serializer: ReservationSerializer
 end
