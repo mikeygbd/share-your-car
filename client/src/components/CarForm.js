44e3b0e148cb892d951c6bc1b7c4e07cb3fc8c99
mikeygbd <mikey.soares@gmail.com>
2019-06-28 09:31:52 -0400

    imported withRouter and created mapStateToProps and returned signupCarFormData

--- client/src/components/CarForm.js
+++ client/src/components/CarForm.js
@@ -6,8 +6,19 @@ import Toolbar from '@material-ui/core/Toolbar';
 import { connect } from 'react-redux';
 import { signupCar } from '../actions/myCars'
 import { updateSignupCarForm } from '../actions/signupCarForm'
+import { withRouter } from 'react-router-dom';
 
 
 const CarForm = ({ history, signupCarFormData, updateSignupCarForm, signupCar}) => {
 
 
+
+
+const mapStateToProps = state => {
+  return {
+  signupCarFormData: state.signupCarForm
+  }
+}
+
+
+export default withRouter(connect(mapStateToProps, { updateSignupCarForm, signupCar })(CarForm))
