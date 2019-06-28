1b1f93d7e5147be8e3bb227996e1fc379d65789d
mikeygbd <mikey.soares@gmail.com>
2019-06-28 09:23:23 -0400

    returned an empty array on clear my cars to clear all the cars when you log out

--- client/src/reducers/myCars.js
+++ client/src/reducers/myCars.js
@@ -2,6 +2,8 @@ export default (state = [], action) => {
   switch(action.type) {
     case "SET_MY_CARS":
     return action.cars
+    case "CLEAR_MY_CARS":
+    return []
     default:
     return state
   }
