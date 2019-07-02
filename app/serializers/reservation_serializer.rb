e7c062dc9be7f2da04f80b39eb05d39de72d38f0
mikeygbd <mikey.soares@gmail.com>
2019-07-01 21:56:17 -0400

    added start_time and end_time to reservations serializer

--- app/serializers/reservation_serializer.rb
+++ app/serializers/reservation_serializer.rb
@@ -1,5 +1,5 @@
 class ReservationSerializer < ActiveModel::Serializer
-  attributes :id, :start_date, :end_date, :created_at, :updated_at, :customer_id, :owner_id, :car_id
+  attributes :id, :start_date, :start_time, :end_date, :end_time, :created_at, :updated_at, :customer_id, :owner_id, :car_id
   belongs_to :car, serializer: CarSerializer
   belongs_to :owner, serializer: OwnerSerializer
   belongs_to :customer, serializer: CustomerSerializer
