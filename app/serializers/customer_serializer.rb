c1eb8ed74f3499af372d848640bfc6ae7bf8000a
mikeygbd <mikey.soares@gmail.com>
2019-07-01 21:55:55 -0400

    added has many reviews to sutomer serializer

--- app/serializers/customer_serializer.rb
+++ app/serializers/customer_serializer.rb
@@ -1,4 +1,5 @@
 class CustomerSerializer < ActiveModel::Serializer
   attributes :id, :email, :firstname, :lastname, :hometown, :img
   has_many :reservations, serializer: ReservationSerializer
+  has_many :reviews, serializer: ReviewSerializer
 end
