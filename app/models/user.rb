10d373b6c1be1f3584cdc6767f0c1662ddb04252
mikeygbd <mikey.soares@gmail.com>
2019-07-01 21:55:00 -0400

    added has many reviews with foreign key customer

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
   has_many :bookings, foreign_key: :owner_id, class_name: "Reservation"
   belongs_to :hometown, class_name: "Location"
   has_many :reservations, foreign_key: :customer_id, class_name: "Reservation"
-  has_many :reviews
+  has_many :reviews, foreign_key: :customer_id, class_name: "Review"
+
 
   validates :firstname, :lastname, :email, :hometown, presence: true
   validates :email, uniqueness: true
