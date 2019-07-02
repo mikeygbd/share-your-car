8c8ef1e89acc80e1323b4dbe66c660d434b5a937
mikeygbd <mikey.soares@gmail.com>
2019-07-01 22:12:35 -0400

    added set new car case that spreads on the state.

--- client/src/reducers/cars.js
+++ client/src/reducers/cars.js
@@ -5,6 +5,8 @@ export default (state = [], action) => {
   switch(action.type) {
     case "SET_CARS":
     return action.cars
+    case "SET_NEW_CAR":
+    return [...state, action.car]
     default:
     return state
   }
