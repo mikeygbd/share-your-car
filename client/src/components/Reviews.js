001d95ea3307731e9bb0f0e6d72b3caec8d609b7
mikeygbd <mikey.soares@gmail.com>
2019-07-01 22:12:04 -0400

    created container for review cards

---
+++ client/src/components/Reviews.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import ReviewCard from './ReviewCard'
+import CarCard from './CarCard'
+
+import { connect } from 'react-redux'
+import Rating from './Rating'
+
+
+const Reviews = ({ reviews}) => {
+  const reviewCards = reviews.length > 0 ? reviews.map(r => <CarCard review={r} key={r.id}/>) : null
+
+  return (
+    <div className="Reviews">
+
+     {reviewCards}
+     </div>
+  )
+}
+
+const mapStateToProps = state => {
+  return {
+    reviews: state.reviews
+  }
+}
+
+export default connect(mapStateToProps)(Reviews)
