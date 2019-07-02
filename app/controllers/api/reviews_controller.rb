a05c283f561e2a3fe9292716e5ab36d044907e9e
mikeygbd <mikey.soares@gmail.com>
2019-07-01 21:54:05 -0400

    review must have a car customer and reservation. so I made sure i did that before i saved the review. added rating and customer_id and reservation_id to params

--- app/controllers/api/reviews_controller.rb
+++ app/controllers/api/reviews_controller.rb
@@ -16,9 +16,13 @@ class Api::ReviewsController < ApplicationController
   # POST /reviews
   def create
     @review = Review.new(review_params)
+    @car = Car.find_by(make: params[:review][:car][:make], model: params[:review][:car][:model], year: params[:review][:car][:year], daily_rate: params[:review][:car][:daily_rate], weekly_rate: params[:review][:car][:weekly_rate], monthly_rate: params[:review][:car][:monthly_rate], img: params[:review][:car][:img], description: params[:review][:car][:description] )
+    @review.customer = current_user
+    @review.car = @car
+    @review.reservation = Reservation.find(@review.reservation_id)
 
     if @review.save
-      render json: @review, status: :created, location: @review
+      render json: @review, status: :created
     else
       render json: @review.errors, status: :unprocessable_entity
     end
@@ -46,6 +50,6 @@ class Api::ReviewsController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def review_params
-      params.require(:review).permit(:user_id, :car_id, :content)
+      params.require(:review).permit(:customer_id, :car_id, :reservation_id, :content, :rating)
     end
 end
