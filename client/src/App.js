95b1d5c29de7251bb4cd5c883d2fdaa3f53ed5a8
mikeygbd <mikey.soares@gmail.com>
2019-07-01 21:58:49 -0400

    imported getReviews, ReviewForm and Reviews. added getReviews to the didmount

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { connect } from 'react-redux'
 import { getCurrentUser } from './actions/currentUser'
 import { getCars } from './actions/cars'
+import { getReviews } from './actions/reviews'
 import NavBar from './components/NavBar'
 import { Switch, Route, NavLink, Link, withRouter } from 'react-router-dom';
 import MyCars from './components/MyCars'
@@ -15,12 +16,16 @@ import Search from './components/Search';
 import MyProfile from './components/MyProfile';
 import CarForm from './components/CarForm';
 import ReservationForm from './components/ReservationForm';
+import ReviewForm from './components/ReviewForm';
+import Reviews from './components/Reviews'
+
 
 
 class App extends React.Component {
 
   componentDidMount() {
     this.props.getCurrentUser()
+    this.props.getReviews()
     this.props.getCars()
 
   }
@@ -34,14 +39,16 @@ class App extends React.Component {
       <Switch>
         <Route exact path='/my_cars' render={props => <MyCars {...props} />}  />
         <Route exact path='/create_reservation' render={props => <ReservationForm {...props} />}  />
+        <Route exact path='/create_review' render={props => <ReviewForm {...props} />}  />
         <Route exact path='/cars' render={props => <Cars {...props} />}  />
+        <Route exact path='/reviews' render={props => <Reviews {...props} />}  />
         <Route exact path='/login' render={props => <Login {...props} />}  />
         <Route exact path='/logout' render={props => <Logout {...props} />}  />
         <Route exact path='/signup' render={props => <UserSignup {...props} />}  />
         <Route exact path='/search' render={props => <Search {...props} />} />
         <Route exact path='/my_profile' render={props => <MyProfile {...props} />}  />
         <Route exact path='/car_form' render={props => <CarForm {...props} />}  />
-        <Route exact path='/delete_car' render={props => <Logout {...props} />}  />
+        <Route exact path='/cars/:id/destroy' render={props => <MyCars {...props} />}  />
 
         <Route component={Home} />
       </Switch>
@@ -54,4 +61,4 @@ class App extends React.Component {
 
 
 
-export default connect(null, {getCurrentUser, getCars})(App);
+export default connect(null, {getCurrentUser, getCars, getReviews })(App);
