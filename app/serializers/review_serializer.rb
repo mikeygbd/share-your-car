a97bfec55fa27020eee1093191fc2c8121b93dbc
mikeygbd <mikey.soares@gmail.com>
2019-07-01 21:56:55 -0400

    added belongs to customer and changed user_id to customer_id

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,6 +1,6 @@
 class ReviewSerializer < ActiveModel::Serializer
-  attributes :id, :content, :car_id, :user_id, :reservation_id
-  belongs_to :user, serializer: UserSerializer
+  attributes :id, :content, :car_id, :customer_id, :reservation_id, :rating
+  belongs_to :customer, serializer: UserSerializer
   belongs_to :car, serializer: CarSerializer
   belongs_to :reservation, serializer: ReservationSerializer
 
