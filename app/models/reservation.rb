311a3d1fd0550eceae00a29aa8692ab0d7a682a2
mikeygbd <mikey.soares@gmail.com>
2019-07-02 08:18:10 -0400

    attempted to validate overlaps

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -4,4 +4,18 @@ class Reservation < ApplicationRecord
   belongs_to :car
   has_one :location, through: :car
   has_many :reviews
+
+ # validates :start_date, :end_date, :overlap => {:message_title => [:start_at, :end_at], :message_content => "Some validation message"}
+
+
+
+ # validate :no_overlap
+ #
+ # def no_overlap
+ #   reservations = Reservation.where(customer_id: customer_id)
+ #   overlap = reservations.select{ |reservation| (start_date - reservation.end_date) * (reservation.start_date - end_date) > 0  }
+ #   unless overlap.blank?
+ #     raise "Conflicting reservation for customer: #{customer_id}"
+ #   end
+ # end
 end
