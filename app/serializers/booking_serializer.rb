d985b0c918dbda3018ae53d13b882ad5071455ea
mikeygbd <mikey.soares@gmail.com>
2019-07-01 21:55:36 -0400

    added start time and end time to booking serializer

--- app/serializers/booking_serializer.rb
+++ app/serializers/booking_serializer.rb
@@ -1,4 +1,4 @@
 class BookingSerializer < ActiveModel::Serializer
-  attributes :id, :start_date, :end_date, :created_at, :updated_at, :customer_id, :owner_id, :car_id
-  
+  attributes :id, :start_date, :start_time, :end_date, :end_time, :created_at, :updated_at, :customer_id, :owner_id, :car_id
+
 end
