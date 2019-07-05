9edce80760c8cf0ea4b98d075483d19131631635
mikeygbd <mikey.soares@gmail.com>
2019-07-05 11:06:35 -0400

    created delete car method but still not working

--- client/src/components/MyCars.js
+++ client/src/components/MyCars.js
@@ -1,11 +1,25 @@
 import React from 'react'
 import CarCard from './CarCard'
 import { connect } from 'react-redux'
-
+import {deleteOwnerCar} from '../actions/myCars';
 
 const MyCars = ({ myCars, currentUser}) => {
-  const carCards = myCars.length > 0 ? myCars.map(c => <CarCard car={c} key={c.id}/>) : null
-  const Title = myCars.length === 0 ? "You Have No cars": null
+
+console.log(myCars)
+  const deleteCar = (id) => {
+
+      myCars = myCars.filter(car => {
+       return car.id !== id
+     })
+      console.log(id)
+      console.log(myCars)
+  }
+
+
+
+
+  // renderTodos = () => this.props.todos.map((todo, id) => <Todo delete={this.props.delete} key={id} text={todo} />)
+  let carCards = myCars.length > 0 ? myCars.map(c => <CarCard deleteCar={deleteCar} car={c} key={c.id}/>) : null
 
   return (
     <div className="MyCars">
@@ -22,5 +36,7 @@ const mapStateToProps = state => {
 
   }
 }
+
+
 
 export default connect(mapStateToProps)(MyCars)
