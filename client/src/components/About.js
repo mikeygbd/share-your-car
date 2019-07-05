a68a985f776fc4431b38314c59f853f5a733d0de
mikeygbd <mikey.soares@gmail.com>
2019-07-05 11:04:14 -0400

    created about page with short description of the app

---
+++ client/src/components/About.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const About = () => {
+  return (
+
+    <div className="About">
+    <div className="container">
+      <div className="d-flex justify-content-center h-100">
+        <div className='about-card'>
+          <h3 className="card-header">About</h3>
+          <p className="about-msg">
+              <strong>Share Your Car</strong> is a simple easy way to rent your car for extra cash. We are an American peer-to-peer carsharing company. We allow private car owners to rent out their vehicles via an online and mobile interface. 
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+)
+};
+
+export default About
