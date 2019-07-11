f2c8965d775c971203f66f32d0e7feac3df5aa2f
mikeygbd <mikey.soares@gmail.com>
2019-07-11 10:06:25 -0400

    created a car container that got the id of the car from the url  and found the specific car to passed it as props to the Car component

---
+++ client/src/components/CarContainer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Car from './Car';
+import { connect } from 'react-redux'
+
+function CarContainer({ match, cars}) {
+  let id = +match.params.carId.replace((/car_/), "")
+  let car;
+
+  if (cars.length > 0 && cars.find(car => car.id === id)) {
+    car = cars.find(car => car.id === id)
+    return <Car car={ car } />
+  }  else {
+    return ""
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+
+    cars: state.cars
+
+  }
+}
+
+
+export default connect(mapStateToProps)(CarContainer);
