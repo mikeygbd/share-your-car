3d7abd372589cd405716eddd05eb924a94c7233b
mikeygbd <mikey.soares@gmail.com>
2019-07-24 08:01:39 -0400

    tried to figure out how to filter using the search bar

--- client/src/components/Cars.js
+++ client/src/components/Cars.js
@@ -2,14 +2,41 @@ import React from 'react'
 import CarCard from './CarCard'
 import VehicleCard from './VehicleCard'
 import { connect } from 'react-redux'
+import Search from './Search';
 
 
 const Cars = ({ cars, reviews}) => {
-  const carCards = cars.length > 0 ? cars.map(c => <CarCard car={c} key={c.id}/>) : null
+  const [filterText, setFilterText] = React.useState({
+    filterText: ''
 
+  })
+  const [filterMake, setFilterMake] = React.useState({
+    filterMake: ''
+  })
+
+  const filterUpdate = (value) => {
+    setFilterText({
+      filterText: value
+    })
+  }
+  const filterUpdateMake = (value) => {
+    setFilterMake({
+      filterMake: value
+    })
+  }
+
+console.log(filterMake)
+
+  const carCards = cars.length > 0 ? cars
+  .map(c => <CarCard car={c}  key={c.id}/>) : null
   return (
+    <div>
+    <div className="searchBar">
+    <Search cars={cars} filterText={filterText} filterUpdateMake={filterUpdateMake.bind()} filterUpdate={filterUpdate.bind()}/>
+    </div>
     <div className="Cars">
      {carCards}
+     </div>
      </div>
   )
 }
