f19faaa747d69bc8c061a5970f501b92dfa70259
mikeygbd <mikey.soares@gmail.com>
2019-08-08 09:36:02 -0400

    adjusted material-ui core imports to one line

--- client/src/components/ReservationCard.js
+++ client/src/components/ReservationCard.js
@@ -1,15 +1,7 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles';
 import clsx from 'clsx';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardActions from '@material-ui/core/CardActions';
-import Collapse from '@material-ui/core/Collapse';
-import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
+import {Card, CardContent, CardHeader, CardMedia, CardActions, Collapse, Avatar, IconButton, Typography,  } from '@material-ui/core';
 import { grey } from '@material-ui/core/colors';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
