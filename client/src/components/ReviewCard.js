061fed848da5eaab2ea89acef252d815504a0bf7
mikeygbd <mikey.soares@gmail.com>
2019-08-08 09:36:12 -0400

    adjusted material-ui core imports to one line

--- client/src/components/ReviewCard.js
+++ client/src/components/ReviewCard.js
@@ -1,10 +1,6 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
+import {Card, CardHeader, Avatar, IconButton, Typography, } from '@material-ui/core';
 import { grey } from '@material-ui/core/colors';
 import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux'
