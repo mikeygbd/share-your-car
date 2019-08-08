b8ce6350e2c496ac77347331c243f47cdf7c503f
mikeygbd <mikey.soares@gmail.com>
2019-08-08 09:36:18 -0400

    adjusted material-ui core imports to one line

--- client/src/components/ReviewForm.js
+++ client/src/components/ReviewForm.js
@@ -1,8 +1,6 @@
 import React from 'react'
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
+import {TextField, Button, Toolbar} from '@material-ui/core';
 import '../styles/registration.css'
-import Toolbar from '@material-ui/core/Toolbar';
 import { connect } from 'react-redux';
 import { createReview } from '../actions/reviews'
 import { updateCreateReviewForm } from '../actions/createReviewForm'
