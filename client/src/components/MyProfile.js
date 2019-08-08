a7f0c95b329221a86291a23209269d022709620a
mikeygbd <mikey.soares@gmail.com>
2019-08-08 09:35:46 -0400

    adjusted material-ui core imports to one line

--- client/src/components/MyProfile.js
+++ client/src/components/MyProfile.js
@@ -1,21 +1,11 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Avatar from '@material-ui/core/Avatar';
-import CardHeader from '@material-ui/core/CardHeader';
-
-import CardActionArea from '@material-ui/core/CardActionArea';
-import Button from '@material-ui/core/Button';
+import {Card, CardContent, CardMedia, Avatar, CardHeader, CardActionArea, Button, CardActions, Typography, Toolbar } from '@material-ui/core';
 import { Link } from 'react-router-dom';
-import CardActions from '@material-ui/core/CardActions';
-import Typography from '@material-ui/core/Typography';
 import { connect } from 'react-redux'
 import CarCard from './CarCard'
 import BookingCard from './BookingCard'
 import ReservationCard from './ReservationCard'
-import Toolbar from '@material-ui/core/Toolbar';
 
 
 
