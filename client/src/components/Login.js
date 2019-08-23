05fa6c85efd57516b98ad085ac6bd8c9da637da3
mikeygbd <mikey.soares@gmail.com>
2019-08-23 07:39:01 -0400

    cleaned up code with auto indent

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -48,45 +48,44 @@ const Login = ({ history, loginForm, updateLoginForm, login }) => {
 
   return (
     <div className="Login">
-    <div className="UserForm">
-      <div className="UserForm-title">
+      <div className="UserForm">
+        <div className="UserForm-title">
 
-      <Toolbar>Log in</Toolbar>
+          <Toolbar>Log in</Toolbar>
 
-      </div>
-    <form id="multi-form" onSubmit={handleSubmit}>
-      <TextField
-        id="standard-with-placeholder"
-        type="text"
-        name="email"
-        placeholder="Email"
-        className={classes.textField}
-        value={loginForm.email}
-        onChange={handleInputChange}
-      />
-    <br />
-      <TextField
-         id="standard-password-input"
-         name="password"
-         className={classes.textField}
-         type="password"
-         placeholder="Password"
-         autoComplete="current-password"
-         value={loginForm.password}
-         onChange={handleInputChange}
-       />
-       <br />
-       <div className="reg-btn">
-     <Button variant="contained" size="small" color="primary" type="submit" onSubmit={handleSubmit} className={classes.margin}>
-            Login
-          </Button>
+        </div>
+        <form id="multi-form" onSubmit={handleSubmit}>
+          <TextField
+            id="standard-with-placeholder"
+            type="text"
+            name="email"
+            placeholder="Email"
+            className={classes.textField}
+            value={loginForm.email}
+            onChange={handleInputChange}
+            />
+          <br />
+          <TextField
+            id="standard-password-input"
+            name="password"
+            className={classes.textField}
+            type="password"
+            placeholder="Password"
+            autoComplete="current-password"
+            value={loginForm.password}
+            onChange={handleInputChange}
+            />
+          <br />
+          <div className="reg-btn">
+            <Button variant="contained" size="small" color="primary" type="submit" onSubmit={handleSubmit} className={classes.margin}>
+              Login
+            </Button>
           </div>
           <br />
 
-    </form>
-  </div>
-  </div>
-
+        </form>
+      </div>
+    </div>
   )
 }
 
